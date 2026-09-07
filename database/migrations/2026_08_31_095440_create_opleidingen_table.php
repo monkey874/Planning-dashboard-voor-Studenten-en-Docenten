5dd69481524f72c57ee08143b400fbac5bca2028
coffee <python.mrk@gmail.com>
2026-09-07 09:25:11 +0200

    afkorting

--- database/migrations/2026_08_31_095440_create_opleidingen_table.php
+++ database/migrations/2026_08_31_095440_create_opleidingen_table.php
@@ -14,7 +14,7 @@ return new class extends Migration
         Schema::create('opleidingen', function (Blueprint $table) {
             $table->id();
             $table->string('naam');
-            $table->string('code', 16)->unique();
+            $table->string('afkorting', 16)->unique();
             $table->timestamps();
         });
     }
