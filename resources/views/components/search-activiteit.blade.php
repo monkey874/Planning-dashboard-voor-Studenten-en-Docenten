3333b855dec3b8c335290803b43b64152e322af4
rickcodemake45 <504848@student.firda.nl>
2026-09-18 12:36:05 +0200

    een werkende versie

--- resources/views/components/search-activiteit.blade.php
+++ resources/views/components/search-activiteit.blade.php
@@ -1,4 +1,4 @@
-<div>
+<div wire:poll.2s="refreshData">
     <div class="flex flex-row justify-center w-full p-4 m-4 items-center">
         <div class="flex flex-row p-3">
 
@@ -24,91 +24,83 @@
 
 
     </div>
+    @props(['projection', 'result', 'Times', 'crudRight'])
 
-    <head>
-        @vite(['resources/css/app.css', 'resources/js/app.js'])
-        @props(['projection', 'result','Times', 'crudRight' ])
+    @php
+    $activityFields = [
+    'titel' => 'ActiviteitTitel',
+    'omschrijving' => 'ActiviteitOmschrijving',
+    'datum' => 'ActiviteitDatum',
+    'starttijd' => 'Activiteitstarttijd',
+    'eindtijd' => 'Activiteiteindtijd',
+    'locatie' => 'ActiviteitLocatie',
+    'type' => 'Activiteiteindtype',
+    'aangemaakt_door_naam' => 'aangemaakt_door_naam',
+    ];
+    @endphp
 
-        @php
-        $activityFields = [
-        'titel' => 'ActiviteitTitel',
-        'omschrijving' => 'ActiviteitOmschrijving',
-        'datum' => 'ActiviteitDatum',
-        'starttijd' => 'Activiteitstarttijd',
-        'eindtijd' => 'Activiteiteindtijd',
-        'locatie' => 'ActiviteitLocatie',
-        'type' => 'Activiteiteindtype',
-        'aangemaakt_door_naam' => 'aangemaakt_door_naam',
-        ];
-        @endphp
+    <div class="p-0 m-0">
+        <table class="w-full h-full">
+            <thead>
+                <tr>
+                    <td>
+                        <x-activitie-border>
+                            <x-activitie-row>
 
-    </head>
+                                @foreach($projection as $fields)
+                                <x-kolum>
+                                    <b>{{ $fields  ?? '' }}</b>
+                                </x-kolum>
+                                @endforeach
 
+                                @if($crudRight)
+                                <x-kolum>
+                                    <b>acties</b>
+                                </x-kolum>
+                                @endif
 
-    <body class="p-0 m-0">
-        <diV>
-            <table class="w-full h-full">
-                <thead>
-                    <tr>
-                        <td>
-                            <x-activitie-border>
-                                <x-activitie-row>
+                            </x-activitie-row>
+                        </x-activitie-border>
+                    </td>
+                </tr>
+            </thead>
 
-                                    @foreach($projection as $fields)
-                                    <x-kolum>
-                                        <b>{{ $fields  ?? '' }}</b>
-                                    </x-kolum>
-                                    @endforeach
+            <tbody>
+                @foreach($result as $slot)
+                <tr>
+                    <td colspan="{{ count($projection) }}">
+                        @if(count($slot['Activiteiten']) > 0)
+                        @foreach($slot['Activiteiten'] as $index => $Activiteit)
 
-                                    @if($crudRight)
-                                    <x-kolum>
-                                        <b>acties</b>
-                                    </x-kolum>
-                                    @endif
+                        <x-activitie-border>
+                            <x-activitie-row>
 
-                                </x-activitie-row>
-                            </x-activitie-border>
-                        </td>
-                    </tr>
-                </thead>
+                                @foreach($projection as $field)
+                                <x-kolum>
+                                    <p>{{ $Activiteit[$activityFields[$field] ?? $field] ?? '' }}</p>
+                                </x-kolum>
+                                @endforeach
 
-                <tbody>
-                    @foreach($result as $slot)
-                    <tr>
-                        <td colspan="{{ count($projection) }}">
-                            @if(count($slot['Activiteiten']) > 0)
-                            @foreach($slot['Activiteiten'] as $index => $Activiteit)
-
-                            <x-activitie-border>
-                                <x-activitie-row>
-
-                                    @foreach($projection as $field)
-                                    <x-kolum>
-                                        <p>{{ $Activiteit[$activityFields[$field] ?? $field] ?? '' }}</p>
-                                    </x-kolum>
-                                    @endforeach
-
-                                    @if($crudRight)
-                                    <x-kolum>
-                                        <div class="gap-4 flex flex-row">
-                                            <x-icon-border>
-                                                <x-hugeicons-edit-01 class="w-6 h-6 text-black-500" />
-                                            </x-icon-border>
-                                            <x-icon-border>
-                                                <x-heroicon-o-trash class="w-6 h-6 text-black-500" />
-                                            </x-icon-border>
-                                        </div>
-                                    </x-kolum>
-                                    @endif
-                                </x-activitie-row>
-                            </x-activitie-border>
-                            @endforeach
-                            @endif
-                        </td>
-                    </tr>
-                    @endforeach
-                </tbody>
-            </table>
-        </div>
-    </body>
+                                @if($crudRight)
+                                <x-kolum>
+                                    <div class="gap-4 flex flex-row">
+                                        <x-icon-border>
+                                            <x-hugeicons-edit-01 class="w-6 h-6 text-black-500" />
+                                        </x-icon-border>
+                                        <x-icon-border>
+                                            <x-heroicon-o-trash class="w-6 h-6 text-black-500" />
+                                        </x-icon-border>
+                                    </div>
+                                </x-kolum>
+                                @endif
+                            </x-activitie-row>
+                        </x-activitie-border>
+                        @endforeach
+                        @endif
+                    </td>
+                </tr>
+                @endforeach
+            </tbody>
+        </table>
+    </div>
 </div>
